fb04db77b447704bdb602a4c3ad4f5e1477a6b33
Snare Li <lsclcl@sina.com>
2023-05-17 19:58:22 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,6 +15,17 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanel title="测试标题" subTitle="测试副标题">
+    <div class="test">
+      测试内容
+    </div>
+  </HomePanel>
+  <HomePanel title="测试标题" subTitle="测试副标题">
+    <div class="test">
+      测试内容
+    </div>
+  </HomePanel>
 </template>
 
 <style lang="scss" scoped>

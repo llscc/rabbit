2019096ead3eef2fbe4b019a06942812a39e055f
Snare Li <lsclcl@sina.com>
2023-05-17 12:10:24 +0800

    finished theme color config

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,25 @@ export default defineConfig({
     resolvers: [ElementPlusResolver()],
   }),
   Components({
-    resolvers: [ElementPlusResolver()],
+    resolvers: [
+      //  指定elementplus采用sass样式配色系统
+      ElementPlusResolver({importStyle: 'sass'})
+    ],
   }),
   ],
-  resolve: {
+  resolve: { // 配置别名
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: { // 配置全局样式
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })

82f801f3348dee0ddd5c1d9d4aa0da3d44256df8
Snare Li <lsclcl@sina.com>
2023-05-18 18:53:00 +0800

    放大镜滑块 设定位置

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted ,watch} from 'vue'
+import { useMouseInElement } from '@vueuse/core';
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -15,6 +16,36 @@ const activeIndex = ref(0)
 const enterhandler = (i) => {
   activeIndex.value = i
 }
+
+// 获取鼠标相对位置
+const target = ref(null)
+const {elementX, elementY, isOutside} = useMouseInElement(target)
+
+// 3.控制滑块跟随鼠标移动，且不超出边界，计算公式：滑块位置 = 鼠标位置 - 滑块宽高的一半
+const left = ref(0)
+const top = ref(0)
+watch([elementX,elementY],()=>{
+    if(elementX.value>100 && elementX.value<300){
+        left.value = elementX.value - 100
+    }
+    if(elementY.value>100 && elementY.value<300){
+        top.value = elementY.value - 100
+    }
+
+    // 处理边界
+    if(elementX.value>300){
+        left.value = 200
+    }
+    if(elementX.value<100){
+        left.value = 0
+    }
+    if(elementY.value>300){
+        top.value = 200
+    }
+    if(elementY.value<100){
+        top.value = 0
+    }
+})
 </script>
 
 
@@ -24,7 +55,7 @@ const enterhandler = (i) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">

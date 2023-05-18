2864e2b76e35f071df36ab5f11da334988ca88bf
Snare Li <lsclcl@sina.com>
2023-05-18 10:59:01 +0800

    路由缓存

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,19 +1,26 @@
 <script setup>
 import { getCategoryAPI } from '@/apis/category';
 import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router'
+import { useRoute, onBeforeRouteUpdate } from 'vue-router'
 import { getBannerAPI } from '@/apis/home'
 import GoodsItem from '@/views/Home/components/GoodsItem.vue'
 
 // 获取数据
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async () => {
-    const res = await getCategoryAPI(route.params.id)
+const getCategory = async (id = route.params.id) => {
+    const res = await getCategoryAPI(id)
     categoryData.value = res.result
 }
 onMounted(() => {
     getCategory()
+})
+
+// 目标：路由参数变化的时候，可以把分类数据接口重新发送
+onBeforeRouteUpdate((to)=>{
+    //console.log('路由参数变化了',route)
+    // 存在问题：路由参数变化的时候，可以把分类数据接口重新发送
+    getCategory(to.params.id)
 })
 
 // 获取banner

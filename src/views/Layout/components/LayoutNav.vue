06641c3e802b0a5e9dc13777627f84141e42212c
Snare Li <lsclcl@sina.com>
2023-05-19 10:58:46 +0800

    使用token适配多模板

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { useUserStore } from '@/stores/user';
+const userStore = useUserStore()
 
 </script>
 
@@ -6,8 +8,9 @@
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <!-- 适配思路：登录后显示第一块，没登陆显示第二块 是否有token  -->
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
